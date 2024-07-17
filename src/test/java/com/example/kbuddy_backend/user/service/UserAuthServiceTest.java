f7ae6d72397b4eb900004c7c4582a8251a9a4c9d
suyong choi <dpfprtus@gmail.com>
2024-07-17 13:18:10 +0900

    [FEAT] OAuth로 가입된 회원인지 테스트

--- src/test/java/com/example/kbuddy_backend/user/service/UserAuthServiceTest.java
+++ src/test/java/com/example/kbuddy_backend/user/service/UserAuthServiceTest.java
@@ -15,6 +15,7 @@ import com.example.kbuddy_backend.user.constant.Gender;
 import com.example.kbuddy_backend.user.constant.OAuthCategory;
 import com.example.kbuddy_backend.user.dto.request.LoginRequest;
 import com.example.kbuddy_backend.user.dto.request.OAuthLoginRequest;
+import com.example.kbuddy_backend.user.dto.request.OAuthLoginRequest;
 import com.example.kbuddy_backend.user.dto.request.OAuthRegisterRequest;
 import com.example.kbuddy_backend.user.dto.request.RegisterRequest;
 import com.example.kbuddy_backend.user.entity.User;
@@ -46,7 +47,7 @@ class UserAuthServiceTest extends IntegrationTest {
     @Test
     void registerSuccess() {
         //given
-        RegisterRequest registerRequest = RegisterRequest.of("test", "test", "test", "test", "test", Country.KOREA,
+        RegisterRequest registerRequest = RegisterRequest.of("test", "test", "test","test","test", Country.KOREA,
                 Gender.M);
         given(userRepository.save(any(User.class))).willReturn(UserFixtures.createUser());
 
@@ -89,7 +90,7 @@ class UserAuthServiceTest extends IntegrationTest {
         //then
         assertThat(register).extracting("accessToken").isNotNull();
         assertThat(register).extracting("refreshToken").isNotNull();
-    }
+     }
 
     @DisplayName("패스워드가 일치하지 않으면 에러를 던진다.")
     @Test
@@ -102,8 +103,8 @@ class UserAuthServiceTest extends IntegrationTest {
 
         //then
         assertThatThrownBy(() -> userAuthService.login(loginRequest))
-                .isInstanceOf(InvalidPasswordException.class)
-                .hasMessageContaining("비밀번호가 일치하지 않습니다.");
+            .isInstanceOf(InvalidPasswordException.class)
+            .hasMessageContaining("비밀번호가 일치하지 않습니다.");
     }
 
 
@@ -111,13 +112,13 @@ class UserAuthServiceTest extends IntegrationTest {
     @Test
     void checkNotFoundUser() {
         //given
-        LoginRequest loginRequest = LoginRequest.of("test", "test");
+        LoginRequest loginRequest = LoginRequest.of("test","test");
         given(userRepository.findByEmail(anyString())).willReturn(Optional.empty());
 
         //then
         assertThatThrownBy(() -> userAuthService.login(loginRequest))
-                .isInstanceOf(UserNotFoundException.class)
-                .hasMessageContaining("사용자를 찾을 수 없습니다.");
+            .isInstanceOf(UserNotFoundException.class)
+            .hasMessageContaining("사용자를 찾을 수 없습니다.");
     }
 
     @DisplayName("중복된 이메일을 확인한다.")
@@ -126,12 +127,24 @@ class UserAuthServiceTest extends IntegrationTest {
         //given
         User user = UserFixtures.createUser();
         RegisterRequest registerRequest = RegisterRequest.of("test", "test", "test", "test", "test", Country.KOREA,
-                Gender.M);
+            Gender.M);
         given(userRepository.findByEmail(anyString())).willReturn(Optional.of(user));
 
         //then
         assertThatThrownBy(() -> userAuthService.register(registerRequest))
-                .isInstanceOf(DuplicateUserException.class)
-                .hasMessageContaining("이미 존재하는 사용자입니다.");
+            .isInstanceOf(DuplicateUserException.class)
+            .hasMessageContaining("이미 존재하는 사용자입니다.");
+    }
+
+    @DisplayName("OAuth로 가입된 회원인지 확인한다.")
+    @Test
+    void checkOAuthRegisterUser() {
+        //given
+        OAuthLoginRequest oAuthLoginRequest = OAuthLoginRequest.of("k-buddy@gmail.com", OAuthCategory.KAKAO);
+        given(userRepository.findByEmailAndOauthCategory(anyString(), any())).willReturn(
+                Optional.of(UserFixtures.createOAuthUser()));
+
+        //then
+        assertThat(userAuthService.checkOAuthUser(oAuthLoginRequest)).isTrue();
     }
 }
