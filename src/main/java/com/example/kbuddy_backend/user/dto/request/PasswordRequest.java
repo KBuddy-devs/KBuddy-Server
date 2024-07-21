9d42a6c30e3ce5032e4363ab3b5767141c4f7116
dpfprtus <dpfprtus@gmail.com>
2024-07-22 00:22:31 +0900

    [FEAT] 비밀번호 변경 테스트

--- src/main/java/com/example/kbuddy_backend/user/dto/request/PasswordRequest.java
+++ src/main/java/com/example/kbuddy_backend/user/dto/request/PasswordRequest.java
@@ -1,4 +1,7 @@
 package com.example.kbuddy_backend.user.dto.request;
 
 public record PasswordRequest(String password) {
+	public static PasswordRequest of(String password) {
+		return new PasswordRequest(password);
+	}
 }
