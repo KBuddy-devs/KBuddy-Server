cd589df3898931fc84e3a10416e75ffee8629111
suyong choi <dpfprtus@gmail.com>
2024-07-22 22:33:01 +0900

    [FIX] 필터 URL

--- src/main/java/com/example/kbuddy_backend/user/dto/request/OAuthRegisterRequest.java
+++ src/main/java/com/example/kbuddy_backend/user/dto/request/OAuthRegisterRequest.java
@@ -4,9 +4,10 @@ import com.example.kbuddy_backend.user.constant.Country;
 import com.example.kbuddy_backend.user.constant.Gender;
 import com.example.kbuddy_backend.user.constant.OAuthCategory;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 
 //todo: validation
-public record OAuthRegisterRequest(String userId, @Email String email, String password, String firstName, String lastName,
+public record OAuthRegisterRequest(@Pattern(regexp = "^[a-zA-Z0-9]{3,20}$",message = "닉네임 유효성 검사 실패") String userId, @Email(message = "이메일 형식에 맞지 않습니다.") String email, String password, String firstName, String lastName,
 								   Country country, Gender gender, OAuthCategory oauth) {
 	public static OAuthRegisterRequest of(String userId, String email, String password, String firstName, String lastName,
                                           Country country, Gender gender, OAuthCategory oauth) {
