c9d766a80e6f4e41e5c834f9e083aedd1e48425b
suyong choi <dpfprtus@gmail.com>
2024-07-29 21:13:12 +0900

    [FEAT] 공통 응답 포맷

--- src/main/java/com/example/kbuddy_backend/common/advice/response/ApiResponse.java
+++ src/main/java/com/example/kbuddy_backend/common/advice/response/ApiResponse.java
@@ -2,18 +2,28 @@ package com.example.kbuddy_backend.common.advice.response;
 
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ApiResponse<T> {
 
-    private LocalDateTime timestamp = LocalDateTime.now();
-    private String status;
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private HttpStatus status;
     private String path;
     private T message;
     private CustomErrorCode code;
 
+    @Builder
+    public ApiResponse(HttpStatus status, String path, T message, CustomErrorCode code) {
+        this.status = status;
+        this.path = path;
+        this.message = message;
+        this.code = code;
+    }
 
 }
